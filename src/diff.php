762efdd04c09591c633cc1de6b091a503af6327b
ini1990 <ini1990g@gmail.com>
2020-06-21 05:35:54 +0300

    refactoring

--- src/diff.php
+++ src/diff.php
@@ -12,35 +12,26 @@ function genDiff($file1, $file2, $format = 'pretty')
 function buildAst($data1, $data2)
 {
     $allKeys = array_unique(array_keys((array)$data1 + (array) $data2));
-    $addDescriptionTree = array_map(function ($key) use ($data1, $data2) {
-        $newValue = $data2->$key ?? '';
-        $oldValue = $data1->$key ?? '';
-        if (!property_exists($data1, $key)) {
-            return makeNode($key, 'added', $newValue, $oldValue);
-        }
-        if (!property_exists($data2, $key)) {
-            return makeNode($key, 'deleted', $newValue, $oldValue);
-        }
-        if (is_object($oldValue) && is_object($newValue)) {
-            $children = buildAst($oldValue, $newValue);
-            return makeNode($key, 'nested', $newValue, $oldValue, $children);
-        }
-        if ($oldValue === $newValue) {
-            return makeNode($key, 'unchanged', $newValue, $oldValue);
-        } else {
-            return makeNode($key, 'changed', $newValue, $oldValue);
-        }
-    }, $allKeys);
-    return $addDescriptionTree;
-}
 
-function makeNode($key, $type, $newValue, $oldValue = null, $children = null)
-{
-    return [
-        'name' => $key,
-        'type' => $type,
-        'oldValue' => $oldValue,
-        'newValue' => $newValue,
-        'children' => $children
-    ];
+    $ast = array_map(function ($key) use ($data1, $data2) {
+        if (is_object($data1->$key ?? '') && is_object($data2->$key ?? '')) {
+            [$type, $children] = ['nested', buildAst($data1->$key, $data2->$key)];
+        } elseif (!property_exists($data2, $key)) {
+            $type = 'deleted';
+        } elseif (!property_exists($data1, $key)) {
+            $type = 'added';
+        } elseif ($data2->$key === $data1->$key) {
+            $type = 'unchanged';
+        } else {
+            $type = 'changed';
+        }
+
+        return ['name' => $key,
+            'type' => $type,
+            'oldValue' => $data1->$key ?? '',
+            'newValue' => $data2->$key ?? '',
+            'children' => $children ?? ''];
+    }, $allKeys);
+
+    return $ast;
 }
