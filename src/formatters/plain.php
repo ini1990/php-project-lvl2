51fc6efb745004a77d7ae62f1571416b393b5523
ini1990 <ini1990g@gmail.com>
2020-06-22 23:54:15 +0300

    style

--- src/formatters/plain.php
+++ src/formatters/plain.php
@@ -5,11 +5,11 @@ namespace Differ\formatters\plain;
 function rend($ast, $parrent = '')
 {
     $result = array_reduce($ast, function ($acc, $node) use ($parrent) {
-        $valueBefore = is_object($node['oldValue']) ? 'complex value' : trim(json_encode($node['oldValue']), '"');
-        $valueAfter = is_object($node['newValue']) ? 'complex value' : trim(json_encode($node['newValue']), '"');
-        $arr = ['added' => "Property '{$parrent}{$node['name']}' was added with value: '{$valueAfter}'",
+        $oldValue = is_object($node['oldValue']) ? 'complex value' : trim(json_encode($node['oldValue']), '"');
+        $newValue = is_object($node['newValue']) ? 'complex value' : trim(json_encode($node['newValue']), '"');
+        $arr = ['added' => "Property '{$parrent}{$node['name']}' was added with value: '{$newValue}'",
             'deleted' => "Property '{$parrent}{$node['name']}' was removed",
-            'changed' => "Property '{$parrent}{$node['name']}' was changed. From '{$valueBefore}' to '{$valueAfter}'"];
+            'changed' => "Property '{$parrent}{$node['name']}' was changed. From '{$oldValue}' to '{$newValue}'"];
 
         return array_merge($acc, [($arr[$node['type']] ?? rend($node['children'], "{$parrent}{$node['name']}."))]);
     }, []);
