96839a2e46c6f8327d47b4b6347fb1dbe8afe41b
ini1990 <ini1990g@gmail.com>
2020-06-27 13:10:29 +0300

    refactoring again

--- src/Diff.php
+++ src/Diff.php
@@ -4,32 +4,38 @@ namespace Differ\Diff;
 
 use function Differ\Parsers\parse;
 
-function genDiff($file1, $file2, $format = 'pretty')
+function genDiff($filePath1, $filePath2, $format = 'pretty')
 {
-    $getData = fn ($file) => parse(file_get_contents(realpath($file)), pathinfo(realpath($file), PATHINFO_EXTENSION));
-    $rend = "\\Differ\\Formatters\\{$format}\\render";
-    return $rend(buildTree($getData($file1), $getData($file2)));
+    $render = "\\Differ\\Formatters\\{$format}\\render";
+    $getExtension = fn ($filePath) => pathinfo(realpath($filePath), PATHINFO_EXTENSION);
+    $getData = fn ($filePath) => parse(file_get_contents(realpath($filePath)), $getExtension($filePath));
+
+    return $render(buildTree($getData($filePath1), $getData($filePath2)));
 }
 
 function buildTree($data1, $data2)
 {
     $allKey = array_unique(array_keys((array) $data1 + (array) $data2));
-    return array_map(fn ($key) => makeNode($key, $data1->$key ?? '', $data2->$key ?? ''), $allKey);
+
+    return array_map(function ($key) use ($data1, $data2) {
+        if (!property_exists($data2, $key)) {
+            return makeNode($key, 'removed', $data1->$key);
+        } elseif (!property_exists($data1, $key)) {
+            return makeNode($key, 'added', '', $data2->$key);
+        } else {
+            [$oldValue, $newValue] = [$data1->$key, $data2->$key];
+            if (is_object($oldValue) && is_object($newValue)) {
+                $children = buildTree($oldValue, $newValue);
+                return makeNode($key, 'nested', $oldValue, $newValue, $children);
+            } else {
+                $type = ($oldValue === $newValue) ? 'unchanged' : 'changed';
+                return makeNode($key, $type, $oldValue, $newValue);
+            }
+        }
+    }, $allKey);
 }
 
-function makeNode($name, $oldValue, $newValue, $children = [])
+function makeNode($name, $type, $oldValue = '', $newValue = '', $children = [])
 {
-    if ($newValue === '') {
-        $type = 'removed';
-    } elseif ($oldValue === '') {
-        $type = 'added';
-    } elseif ($newValue === $oldValue) {
-        $type = 'unchanged';
-    } elseif (is_object($oldValue) && is_object($newValue)) {
-        $type = 'nested';
-        $children = buildTree($oldValue, $newValue);
-    } else {
-        $type = 'changed';
-    }
     return compact('name', 'type', 'oldValue', 'newValue', 'children');
 }
