d5e619c5f5dbe0ba8ee72e0a0127c2ffef1cc19c
ini1990 <ini1990g@gmail.com>
2020-06-30 15:36:03 +0300

    refactor

--- src/Formatters/pretty.php
+++ src/Formatters/pretty.php
@@ -12,41 +12,37 @@ function render($tree)
 function renderTree($tree, $depth = 0)
 {
     $indent = str_repeat('    ', $depth);
-    $renderedData = Collection\flatten(array_map(fn ($node) => formatNode($node, $depth), $tree));
+    $renderedData = Collection\flatten(array_map(function ($node) use ($depth) {
+        switch ($node['type']) {
+            case "unchanged":
+                return stringify(' ', $node['name'], formatValue($node['oldValue'], $depth));
+            case "added":
+                return stringify('+', $node['name'], formatValue($node['newValue'], $depth));
+            case "removed":
+                return stringify('-', $node['name'], formatValue($node['oldValue'], $depth));
+            case "changed":
+                return  [stringify('+', $node['name'], formatValue($node['newValue'], $depth)),
+                stringify('-', $node['name'], formatValue($node['oldValue'], $depth))];
+            case "nested":
+                return stringify(' ', $node['name'], renderTree($node['children'], $depth + 1));
+            default:
+                throw new \Exception("Unknown type: '{$node['type']}'!");
+        }
+    }, $tree));
     return "{\n{$indent}" . implode("\n{$indent}", $renderedData) . "\n{$indent}}";
-}
-
-function formatNode($node, $depth)
-{
-    if (!key_exists('type', $node)) {
-        return getText(" ", key($node), current($node));
-    }
-    switch ($node['type']) {
-        case "unchanged":
-            return getText(' ', $node['name'], formatValue($node['oldValue'], $depth));
-        case "added":
-            return getText('+', $node['name'], formatValue($node['newValue'], $depth));
-        case "removed":
-            return getText('-', $node['name'], formatValue($node['oldValue'], $depth));
-        case "changed":
-            return  [getText('+', $node['name'], formatValue($node['newValue'], $depth)),
-                    getText('-', $node['name'], formatValue($node['oldValue'], $depth))];
-        case "nested":
-            return getText(' ', $node['name'], renderTree($node['children'], $depth + 1));
-        default:
-            throw new \Exception("Unknown type: '{$node['type']}'!");
-    }
 }
 
 function formatValue($data, $depth)
 {
     if (is_array($data)) {
-        return renderTree(array_chunk($data, 1, true), $depth + 1);
+        $indent = str_repeat('    ', ++$depth);
+        $renderedData = array_map(fn($node) => stringify(' ', key($node), current($node)), array_chunk($data, 1, true));
+        return "{\n{$indent}" . implode("\n{$indent}", $renderedData) . "\n{$indent}}";
     }
     return trim(json_encode($data), '"');
 }
 
-function getText($sign, $key, $value)
+function stringify($sign, $key, $value)
 {
     return sprintf('%3s %s: %s', $sign, $key, $value);
 }
